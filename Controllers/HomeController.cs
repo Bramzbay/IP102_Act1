a07623fba820efba35b57085adc596de7c62c3fd
Baybayon, Bramie R <baybayon.bramie.05141977@gmail.com>
2023-09-23 13:18:10 +0800

    Updated Student List

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,9 @@ public class HomeController : Controller
     {
         var studentInfoArray = new[]
         {
-            new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
-            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
-            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+            new StudentInfoModel { Name = "Bramie Baybayon", StudentId = "S001", StudentAge = "16" },
+            new StudentInfoModel { Name = "Danilo Mirador", StudentId = "S002", StudentAge = "18" },
+            new StudentInfoModel { Name = "Therese Santos", StudentId = "S003", StudentAge = "22" }
         };
 
         return View(studentInfoArray);
